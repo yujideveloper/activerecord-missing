3bd732ae2ff2c5e38f3f752d65727fba9ee82e7c
Yuji Hanamura <yuji.developer@gmail.com>
2020-01-14 22:42:21 +0900

    Initial commit

---
+++ lib/activerecord/missing.rb
@@ -0,0 +1,8 @@
+require "activerecord/missing/version"
+
+module ActiveRecord
+  module Missing
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/activerecord/missing/version.rb
@@ -0,0 +1,5 @@
+module ActiveRecord
+  module Missing
+    VERSION = "0.1.0"
+  end
+end
